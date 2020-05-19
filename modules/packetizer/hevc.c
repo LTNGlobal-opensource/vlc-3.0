d13741b7d98230bc7984501b9ce04d78a29757ce
Steven Toth <stoth@kernellabs.com>
2020-05-19 17:30:50 -0400

    Allow playback of Ateme and OBE HEVC streams that lack SEI recovery point
    Author: Steven Toth <stoth@ltnglobal.com>
    
    Implement routine such that we rely on the presence of SPS data
    irregardless of whether we find an SEI recovery point.
    
    Tested with ateme_cm5000_hevc_1080i59_9.3_ull.ts and LTN's OBE
    release that includes HEVC support.

--- modules/packetizer/hevc.c
+++ modules/packetizer/hevc.c
@@ -51,12 +51,18 @@
 static int  Open (vlc_object_t *);
 static void Close(vlc_object_t *);
 
+#define INTRA_TEXT N_("Enable decoding of HEVC P/B-frame only streams")
+#define INTRA_LONGTEXT N_(\
+    "Some low latency HEVC video encoders don't produce iframes. " \
+    "Enable this if you have HEVC that will not decode")
+
 vlc_module_begin ()
     set_category(CAT_SOUT)
     set_subcategory(SUBCAT_SOUT_PACKETIZER)
     set_description(N_("HEVC/H.265 video packetizer"))
     set_capability("packetizer", 50)
     set_callbacks(Open, Close)
+    add_bool("accept-hevc-intra", true, INTRA_TEXT, INTRA_LONGTEXT, false)
 vlc_module_end ()
 
 
@@ -760,6 +766,9 @@ static block_t * ParseAUHead(decoder_t *p_dec, uint8_t i_nal_type, block_t *p_na
         case HEVC_NAL_SPS:
         case HEVC_NAL_PPS:
         {
+            if (!p_sys->b_init_sequence_complete) {
+                p_sys->b_init_sequence_complete = var_InheritBool(p_dec, "accept-hevc-intra");
+            }
             uint8_t i_id;
             const uint8_t *p_xps = p_nalb->p_buffer;
             size_t i_xps = p_nalb->i_buffer;
