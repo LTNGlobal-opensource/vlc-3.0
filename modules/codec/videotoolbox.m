d4d873970375eca2b32def348515740d31d41350
Graeme Blackley <graeme@devnz.com>
2020-06-20 06:36:11 +1200

    Removed requirement for keyframes with HEVC content in videotoolbox
    This a temporary work around. The code has been left in place for us to revisit later.

--- modules/codec/videotoolbox.m
+++ modules/codec/videotoolbox.m
@@ -1434,7 +1434,7 @@ static int OpenDecoder(vlc_object_t *p_this)
             p_sys->pf_get_extradata = GetDecoderExtradataHEVC;
             p_sys->pf_fill_reorder_info = FillReorderInfoHEVC;
             p_sys->b_poc_based_reorder = true;
-            p_sys->b_vt_need_keyframe = true;
+            //p_sys->b_vt_need_keyframe = true;
             break;
 
         case kCMVideoCodecType_MPEG4Video:
